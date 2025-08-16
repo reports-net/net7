257a2c5990b862267082255ae8b1c97e419e6354
paopapa <pao@pao.ac>
2025-08-17 03:25:22 +0900

    Add initial project files and structure

---
+++ 03.帳票WEBアプリケーション－各種クラウド公開動作中/CS/PaoWebApp/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// EventLogプロバイダーを明示的に除外
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    //app.UseExceptionHandler("/Home/Error");
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
